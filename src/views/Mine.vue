d809886bacdcf25169f187644bd61f09eae81bdb
刘子铭 <86124405@qq.com>
2019-06-11 03:31:10 +0800

    wip: 初始化我的页面

---
+++ src/views/Mine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Mine">mine</div>
+</template>
+
+<script>
+export default {
+  name: 'Mine',
+};
+</script>
+
+<style lang="scss" scoped>
+.Mine {
+  height: 100%;
+}
+</style>
