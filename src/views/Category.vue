d4917b48563cec4ab79e6d658fa060f35cf9e56e
刘子铭 <86124405@qq.com>
2019-06-11 03:30:16 +0800

    feat: 初始化类别页面

---
+++ src/views/Category.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Category">Category</div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+};
+</script>
+
+<style lang="scss" scoped>
+.Category {
+  height: 100%;
+}
+</style>
