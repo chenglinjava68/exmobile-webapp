429657e498a3b8254da01147ee2502b5f65aafb0
刘子铭 <86124405@qq.com>
2019-06-11 03:24:46 +0800

    config: 添加我的页面路由， 代码分割

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,16 +9,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "Home" */ './views/Home'),
+    },
+    {
+      path: '/mine',
+      name: 'Mine',
+      component: () => import(/* webpackChunkName: "Mine" */ './views/Mine'),
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      name: 'About',
+      component: () => import(/* webpackChunkName: "About" */ './views/About'),
     },
   ],
 });
