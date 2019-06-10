cbf1df185e86d1639fd9da7eace9f55e499d7d59
刘子铭 <86124405@qq.com>
2019-06-11 03:30:33 +0800

    wip: 初始化拼团列表页面

---
+++ src/views/Group.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Group">Group</div>
+</template>
+
+<script>
+export default {
+  name: 'Group',
+};
+</script>
+
+<style lang="scss" scoped>
+.Group {
+  height: 100%;
+}
+</style>
