72a71f6451214c6673b55982075bb0a78c09a309
刘子铭 <86124405@qq.com>
2019-06-11 03:21:51 +0800

    config: 配置vw适配，字体库自动引入

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
+    'postcss-font-magician': {
+      protocol: 'https:',
+      foundries: ['google'],
+    },
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      selectorBlackList: ['.ignore', '.hairlines'],
+      minPixelValue: 1,
+      mediaQuery: false,
+      exclude: /node_modules/,
+    },
+    'postcss-aspect-ratio-mini': {},
   },
 };
