ac41b4fab7d9a694d3238e9810cbc2da48489fe2
刘子铭 <86124405@qq.com>
2019-06-11 21:58:08 +0800

    init: 初始化store

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable global-require */
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
+import state from './state';
+import actions from './actions';
+import mutations from './mutations';
+import * as modules from './modules';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+const store = new Vuex.Store({
+  modules,
+  state,
+  mutations,
+  actions,
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
+});
+
+if (module.hot) {
+  module.hot.accept(['./mutations', './state', './actions', './modules'], () => {
+    const newState = require('./state').default;
+    const newActions = require('./actions').default;
+    const newMutations = require('./mutations').default;
+    const { Home } = require('./modules');
+    store.hotUpdate({
+      state: newState,
+      mutations: newMutations,
+      actions: newActions,
+      modules: {
+        Home,
+      },
+    });
+  });
+}
+
+export default store;
