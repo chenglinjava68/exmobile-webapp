fdce90760979d186e21b644116bd001b0d752deb
刘子铭 <86124405@qq.com>
2019-06-11 21:58:56 +0800

    wip: 引入Tabbar组件, 同步router到store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
+import { Tabbar, TabbarItem } from 'vant';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+sync(store, router);
+
+Vue.use(Tabbar).use(TabbarItem);
 
 new Vue({
   router,
