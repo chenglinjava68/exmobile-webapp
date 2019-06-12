f606878c55dd8536b323fca2b8222e26cc278351
刘子铭 <86124405@qq.com>
2019-06-12 23:19:53 +0800

    wip: 完成Tabbar组件的业务逻辑

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="Tabbar" v-show="showTabbar">
+  <div class="Tabbar" v-show="show">
     <van-tabbar v-model="active" route safe-area-inset-bottom>
       <van-tabbar-item
         v-for="item of tabbarItemList"
         :key="item.name"
         :icon="item.icon"
         :to="item.to"
+        :replace="item.replace"
       >
         {{ item.name }}
       </van-tabbar-item>
@@ -14,64 +15,48 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
+import { TABBAR_CHANGED_ACTIVE, TABBAR_CHANGED_SHOW } from '@/constant/MutationType'
+
 export default {
   name: 'Tabbar',
-  beforeRouteUpdate (to, from, next) {
-    console.log(11)
-    this.showTabbar = this.tabbarItemList.some(item => item.to === this.$route.path)
-    next()
-  },
-  data () {
-    return {
-      active: 0,
-      showTabbar: false,
-      tabbarItemList: [
-        {
-          name: '主页',
-          icon: 'home-o',
-          dot: false,
-          info: '',
-          to: '/',
-          replace: true
-        },
-        {
-          name: '分类',
-          icon: 'home-o',
-          dot: false,
-          info: '',
-          to: '/category',
-          replace: true
-        },
-        {
-          name: '拼团',
-          icon: 'home-o',
-          dot: false,
-          info: '',
-          to: '/group',
-          replace: true
-        },
-        {
-          name: '购物车',
-          icon: 'home-o',
-          dot: false,
-          info: '',
-          to: '/shopcart',
-          replace: true
-        },
-        {
-          name: '我的',
-          icon: 'home-o',
-          dot: false,
-          info: '',
-          to: '/mine',
-          replace: true
-        }
-      ]
+  computed: {
+    ...mapState({
+      show: state => state.Tabbar.show,
+      tabbarItemList: state => state.Tabbar.tabbarItemList
+    }),
+    active: {
+      get () {
+        return this.$store.state.Tabbar.active
+      },
+      set (active) {
+        this.TABBAR_CHANGED_ACTIVE({
+          type: TABBAR_CHANGED_ACTIVE,
+          active
+        })
+      }
     }
+  },
+  created () {
+    this.TABBAR_CHANGED_SHOW({
+      type: TABBAR_CHANGED_SHOW,
+      show: this.tabbarItemList.some(item => item.to === this.$route.path)
+    })
+  },
+  methods: {
+    ...mapMutations([
+      TABBAR_CHANGED_ACTIVE,
+      TABBAR_CHANGED_SHOW
+    ])
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.Tabbar {
+  box-sizing: content-box;
+  .van-tabbar {
+    box-sizing: inherit;
+  }
+}
 </style>
