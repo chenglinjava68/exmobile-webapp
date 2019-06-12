494f8a192e52aab6ae71c284a624dd349fbad95a
刘子铭 <86124405@qq.com>
2019-06-12 23:18:55 +0800

    feature: 初始化Tabbar组件state

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,48 @@
 export default {
-
+  Tabbar: {
+    active: 0,
+    show: false,
+    tabbarItemList: [
+      {
+        name: '主页',
+        icon: 'home-o',
+        dot: false,
+        info: '',
+        to: '/',
+        replace: true
+      },
+      {
+        name: '分类',
+        icon: 'home-o',
+        dot: false,
+        info: '',
+        to: '/category',
+        replace: true
+      },
+      {
+        name: '拼团',
+        icon: 'home-o',
+        dot: false,
+        info: '',
+        to: '/group',
+        replace: true
+      },
+      {
+        name: '购物车',
+        icon: 'home-o',
+        dot: false,
+        info: '',
+        to: '/shopcart',
+        replace: true
+      },
+      {
+        name: '我的',
+        icon: 'home-o',
+        dot: false,
+        info: '',
+        to: '/mine',
+        replace: true
+      }
+    ]
+  }
 }
